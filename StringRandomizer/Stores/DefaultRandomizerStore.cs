43d135231068e6f5a4413bab90c2e76513541fb4
Sarthak Mahapatra <>
2018-11-13 12:44:02 +0530

    fixed warnings

--- StringRandomizer/Stores/DefaultRandomizerStore.cs
+++ StringRandomizer/Stores/DefaultRandomizerStore.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 
 namespace StringRandomizer.Stores
 {
+    /// <summary>
+    /// Default randomizer store.
+    /// </summary>
     public class DefaultRandomizerStore : IRandomizerStore
     {
         List<string> _store;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringRandomizer.Stores.DefaultRandomizerStore"/> class.
+        /// </summary>
         public DefaultRandomizerStore()
         {
             _store = new List<string>();
         }
 
+        /// <summary>
+        /// Checks if key exisit in store, if not adds it 
+        /// </summary>
+        /// <returns><c>true</c>, if add was tryed, <c>false</c> otherwise.</returns>
+        /// <param name="key">Key.</param>
         public bool TryAdd(string key)
         {
             lock (_store)
--- StringRandomizer/Stores/IRandomizerStore.cs
+++ StringRandomizer/Stores/IRandomizerStore.cs
@@ -2,8 +2,16 @@
 
 namespace StringRandomizer.Stores
 {
+    /// <summary>
+    /// Randomizer store.
+    /// </summary>
     public interface IRandomizerStore
     {
+        /// <summary>
+        /// Checks if key exisit in store, if not adds it 
+        /// </summary>
+        /// <returns><c>true</c>, if add was tryed, <c>false</c> otherwise.</returns>
+        /// <param name="key">Key.</param>
         bool TryAdd(string key);
     }
 }
