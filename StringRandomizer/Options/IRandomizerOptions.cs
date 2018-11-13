924cbc175c753e52081ef18fadc2b22977878b21
Sarthak Mahapatra <sarthakmahapatra@hotmail.com>
2018-11-13 12:45:45 +0530

    fixed warnings

--- StringRandomizer/Options/IRandomizerOptions.cs
+++ StringRandomizer/Options/IRandomizerOptions.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 namespace StringRandomizer.Options
 {
+    /// <summary>
+    /// Randomizer options.
+    /// </summary>
     public interface IRandomizerOptions
     {
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="StringRandomizer.Options.IRandomizerOptions"/> has numbers.
+        /// </summary>
+        /// <value><c>true</c> if has numbers; otherwise, <c>false</c>.</value>
         bool HasNumbers { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="StringRandomizer.Options.IRandomizerOptions"/> has lower alphabets.
+        /// </summary>
+        /// <value><c>true</c> if has lower alphabets; otherwise, <c>false</c>.</value>
         bool HasLowerAlphabets { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="StringRandomizer.Options.IRandomizerOptions"/> has upper alphabets.
+        /// </summary>
+        /// <value><c>true</c> if has upper alphabets; otherwise, <c>false</c>.</value>
         bool HasUpperAlphabets { get; }
     }
 }
